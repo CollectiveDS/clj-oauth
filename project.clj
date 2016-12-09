3651479edd44a4f4a8d0a976db9d31ba46cd2d83
Mike Flynn <mfflynn@gmail.com>
2016-12-08 16:39:56 -0800

    Applied forked project file for cdshub.

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject clj-oauth "1.5.6-SNAPSHOT"
+(defproject clj-oauth "1.5.2-cds"
   :url "https://github.com/mattrepl/clj-oauth"
-  :license {:name "Simplified BSD License"
-            :url "https://opensource.org/licenses/BSD-2-Clause"
-            :distribution :repo}
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :description "OAuth support for Clojure"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [commons-codec/commons-codec "1.8"]
-                 [org.bouncycastle/bcprov-jdk15on "1.54"]
-                 [org.bouncycastle/bcpkix-jdk15on "1.54"]
-                 [clj-http "2.0.1"]])
+                 [org.bouncycastle/bcprov-jdk15on "1.52"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.52"]
+                 [clj-http "1.0.1"]])
